3d4d1ed164b0db39ad7f5377874765237fa9cc6f
Tj <64882765+singymingy@users.noreply.github.com>
2020-11-17 14:57:55 +1300

    Update asg_scalling_group.tf

--- terraform/nodes-elb/asg_scalling_group.tf
+++ terraform/nodes-elb/asg_scalling_group.tf
@@ -2,9 +2,9 @@ resource "aws_autoscaling_group" "my_asg" {
   launch_configuration = aws_launch_configuration.my-asg-launch-conf.id
   availability_zones   = var.azs
   load_balancers       = [aws_elb.web_elb.name]
-  min_size             = 3
-  max_size             = 6
-  desired_capacity     = 3
+  min_size             = 6
+  max_size             = 10
+  desired_capacity     = 8
 
   tag {
     key                 = "Name"
